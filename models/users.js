80512376fe7821b99e44be9c60def4947e20cb31
Mahmoud <mahmoud.acm.123@gmail.com>
2020-03-27 23:10:00 +0200

    adding security to the password

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 const Post = require('./posts')
 
 const UserSchema = new mongoose.Schema({
@@ -25,6 +26,16 @@ UserSchema.virtual('posts', {
     ref: 'Post',
     localField:'_id',
     foreignField:'userId'
+})
+
+UserSchema.pre('save', async function(next){
+    const user = this 
+    
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
 })
 
 const User = mongoose.model('User', UserSchema)
